33615bf28106f5e5fbac5d922dc5827e4a299076
Ba7arathinam <bala01225@gmail.com>
2024-05-13 17:16:34 +0530

    update add to cart

--- server.js
+++ server.js
@@ -1,156 +1,184 @@
-const express=require('express');
-const app=express()
-const cors=require('cors');
+const express = require('express');
+const app = express()
+const cors = require('cors');
 const { foodCart, foodproduct, sequelize, foodUsers } = require('./ORM');
 const { UpdateCartService } = require('./updateCartService');
 app.use(express.json())
 app.use(cors())
-const multer=require('multer');
+const multer = require('multer');
+const { json } = require('sequelize');
+const { log } = require('handlebars');
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'uploads/')
-    }, 
+        cb(null, 'uploads/')
+    },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
-      }
-    })
+    }
+})
 const upload = multer({ storage: storage })
 
-app.post('/imageupload',upload.single('images'),async function(req,res,next){
+app.post('/imageupload', upload.single('images'), async function (req, res, next) {
     await sequelize.sync();
-    let images=req.file.originalname;
-    let id=req.body.id;
+    let images = req.file.originalname;
+    let id = req.body.id;
     console.log(req['body']);
 
-     await foodproduct.update({ "Product_image":images}, {
+    await foodproduct.update({ "Product_image": images }, {
         where: {
-            "id":id,
+            "id": id,
         },
     });
     console.log("bye")
-    res.json({"message":"data updated"})
+    res.json({ "message": "data updated" })
 })
 
-app.get('/getImage', async(req,res)=>{
+app.get('/getImage', async (req, res) => {
     console.log("hello")
     await sequelize.sync();
-    let id=req['query']['id'];
+    let id = req['query']['id'];
     console.log(id)
-    let result=await foodproduct.findOne({where: 
-        {id:`${id}`}
+    let result = await foodproduct.findOne({
+        where:
+            { id: `${id}` }
     });
     console.log(result)
-    if(result.dataValues.Product_image==null){
-        res.sendFile(`uploads/no_img.jpg`,{root:__dirname})
+    if (result.dataValues.Product_image == null) {
+        res.sendFile(`uploads/no_img.jpg`, { root: __dirname })
     }
-    else{
-        res.sendFile(`uploads/${result.dataValues.Product_image}`,{root:__dirname})
+    else {
+        res.sendFile(`uploads/${result.dataValues.Product_image}`, { root: __dirname })
     }
 })
 
 
 app.get('/getProduct', async (req, res) => {
     await sequelize.sync();
-    let result=await foodproduct.findAll();
-    result.forEach(i=>console.log(i.dataValues));
-    res.json({message:result})
+    let result = await foodproduct.findAll();
+    result.forEach(i => console.log(i.dataValues));
+    res.json({ message: result })
 
 })
 
 //addCart
-app.post('/addCart/:u_id/:p_id',async(req,res)=>{
-   let    user_id=req.params.u_id
-   let  product_id=req.params.p_id
+app.post('/addCart/:u_id/:p_id', async (req, res) => {
+    let user_id = req.params.u_id
+    let product_id = req.params.p_id
     await sequelize.sync();
-    let f_product=await foodproduct.findByPk(product_id)
-    let f_user=await foodUsers.findByPk(user_id)
-   try {
-    if(f_product){
-        if(f_user){
-            let f_Cart=await foodCart.create({
-                p_id:product_id,
-                u_id:user_id,
-                p_name:f_product.dataValues.Product_name,
-                quantity:f_product.dataValues.Quantity,
-                total_price:f_product.dataValues.Price
-            })
-         res.status(200).json(f_Cart)
-        }else{
-            throw 'User Not found,Please Login'
-        }
+    let f_product = await foodproduct.findByPk(product_id)
+    let f_user = await foodUsers.findByPk(user_id)
+    try {
+        if (f_product) {
+            if (f_user) {
+                let cart = await foodCart.findAll({
+                    where: {
+                        p_id: product_id,
+                        u_id: user_id
+                    }
+                });
 
-    }else{
-      throw 'Product Id Not match'
+                if (cart.length > 0) {
+                    let qty = cart[0].dataValues.quantity;
+                    qty++;
+                    let total_amount = qty * cart[0].dataValues.total_price
+                    await foodCart.update(
+                        { quantity: qty, total_price: total_amount },
+                        {
+                            where: {
+                                id: cart[0].dataValues.id
+                            }
+                        }
+                    );
+
+                    let updatedCart = await foodCart.findByPk(cart[0].dataValues.id);
+                    res.status(200).json(updatedCart);
+                } else {
+                    // res.json('new data')
+                    let f_Cart = await foodCart.create({
+                        p_id: product_id,
+                        u_id: user_id,
+                        p_name: f_product.dataValues.Product_name,
+                        quantity: f_product.dataValues.Quantity,
+                        total_price: f_product.dataValues.Price
+                    })
+                    res.status(200).json(f_Cart)
+                }
+
+            } else {
+                throw 'User Not found,Please Login'
+            }
+
+        } else {
+            throw 'Product Id Not match'
+        }
+    } catch (e) {
+        res.status(400).json(e)
     }
-   } catch (e) {
-    res.status(400).json(e)
-   }
 })
 //getCart
 app.get('/getCart/:id', async (req, res) => {
-    try{
-           let uid = req['params']['id'];
-           console.log(uid);
-          let res1 = await foodCart.findAll({ where: { u_id: uid } });
-           console.log(res1);
-           if (res1 == null) {
-               throw "your cart is empty" 
-           }
-           else {
-                res.json({meals:res1})
-           }
-       }
-       catch(e)
-       {
-           res.status(400)
-          console.log(e)
-          res.json({"error":e})
-       }
-       
-   })
+    try {
+        let uid = req['params']['id'];
+        console.log(uid);
+        let res1 = await foodCart.findAll({ where: { u_id: uid } });
+        console.log(res1);
+        if (res1 == null) {
+            throw "your cart is empty"
+        }
+        else {
+            res.json({ meals: res1 })
+        }
+    }
+    catch (e) {
+        res.status(400)
+        console.log(e)
+        res.json({ "error": e })
+    }
 
-   //deleteCart
-   app.delete('/deleteCart',async(req,res)=>{
- 
-    let UserId=req.query.u_id;
-    let product_id=req.query.p_id;
-   
-    console.log(UserId,product_id)
-    try{
+})
+
+//deleteCart
+app.delete('/deleteCart', async (req, res) => {
+
+    let UserId = req.query.u_id;
+    let product_id = req.query.p_id;
+
+    console.log(UserId, product_id)
+    try {
         await sequelize.sync();
 
         await foodCart.destroy({
-            
-            where:{
-                p_id:product_id,
-                u_id:UserId
+
+            where: {
+                p_id: product_id,
+                u_id: UserId
             }
-        });  
-;
-        res.json({message:"item removed from cart"});
+        });
+        ;
+        res.json({ message: "item removed from cart" });
     }
-    catch(e){
+    catch (e) {
         console.log(e);
         res.status(400);
-        res.json({"erroe":"sdsdsd"});
+        res.json({ "erroe": "sdsdsd" });
     }
 
 })
 //update cart
 let service = new UpdateCartService()
 // sample model of endpoint for update "http://localhost:8080/updateCart?p_id=3&qty=6&u_id=3"
-app.post('/updateCart',async (req,res)=>{
+app.post('/updateCart', async (req, res) => {
     try {
         let query = req['query'];
-        let result = await service.updateCart(query['p_id'],query['qty'],query['u_id']);
+        let result = await service.updateCart(query['p_id'], query['qty'], query['u_id']);
         // console.log("RESULT API",result);
-        res.json({"Message":result})
+        res.json({ "Message": result })
     } catch (error) {
         res.json(error.message)
     }
 });
-   
-app.listen(8080,()=>{
+
+app.listen(8080, () => {
     console.log('server is running')
 }
 )
